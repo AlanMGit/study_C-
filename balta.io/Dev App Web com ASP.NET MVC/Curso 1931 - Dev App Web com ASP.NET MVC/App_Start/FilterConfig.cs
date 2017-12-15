56baf31cd0d80f86ef6f340ac88f8c98438b1af5
Alan Martins <alan.m21@hotmail.com>
2017-12-15 20:30:00 -0200

    Initial commit

---
+++ balta.io/Dev App Web com ASP.NET MVC/Curso 1931 - Dev App Web com ASP.NET MVC/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Curso_1931___Dev_App_Web_com_ASP.NET_MVC
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ balta.io/Dev App Web com ASP.NET MVC/Curso 1931 - Dev App Web com ASP.NET MVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Curso_1931___Dev_App_Web_com_ASP.NET_MVC.MvcApplication" Language="C#" %>
