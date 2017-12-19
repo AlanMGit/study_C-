0f8e68350200298b8f18fd83ef1d66555b03e7d4
Alan Martins <alan.m21@hotmail.com>
2017-12-19 09:24:29 -0200

    Created controller, model and added EntityFramework

---
+++ balta.io/App ASP MVC Entity Framework/Dev_app_web_asp_MVC_entity_framework/Models/DevAppContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Dev_app_web_asp_MVC_entity_framework.Models
+{
+    public class DevAppContext : DbContext
+    {
+        // You can add custom code to this file. Changes will not be overwritten.
+        // 
+        // If you want Entity Framework to drop and regenerate your database
+        // automatically whenever you change your model schema, please use data migrations.
+        // For more information refer to the documentation:
+        // http://msdn.microsoft.com/en-us/data/jj591621.aspx
+    
+        public DevAppContext() : base("name=AppContext")
+        {
+        }
+
+        public System.Data.Entity.DbSet<Dev_app_web_asp_MVC_entity_framework.Models.Product> Products { get; set; }
+    }
+}
---
+++ balta.io/App ASP MVC Entity Framework/Dev_app_web_asp_MVC_entity_framework/Models/Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Dev_app_web_asp_MVC_entity_framework.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+        public int Title { get; set; }
+    }
+}
