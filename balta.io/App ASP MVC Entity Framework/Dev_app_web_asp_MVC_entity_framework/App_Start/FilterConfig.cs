e03e5801fbadd716aea9504df4ad03c1c22c2b5a
Alan Martins <alan.m21@hotmail.com>
2017-12-19 09:13:29 -0200

    Initial Commit

---
+++ balta.io/App ASP MVC Entity Framework/Dev_app_web_asp_MVC_entity_framework/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Dev_app_web_asp_MVC_entity_framework
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ balta.io/App ASP MVC Entity Framework/Dev_app_web_asp_MVC_entity_framework/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Dev_app_web_asp_MVC_entity_framework.MvcApplication" Language="C#" %>
