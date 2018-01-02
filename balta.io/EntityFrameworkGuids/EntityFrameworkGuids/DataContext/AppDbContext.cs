951c297bfa95462c52cae5dd4b8dec3b8a1045bc
Alan Martins <alan.m21@hotmail.com>
2018-01-02 13:33:58 -0200

    Example Guids

---
+++ balta.io/EntityFrameworkGuids/EntityFrameworkGuids/DataContext/AppDbContext.cs
@@ -0,0 +1,32 @@
+﻿using EntityFrameworkGuids.Mapping;
+using EntityFrameworkGuids.Model;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFrameworkGuids.DataContext
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext() : base("AppConnectionString")
+        {
+            Database.SetInitializer<AppDbContext>(new AppDbContextInitializer());
+        }
+
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Configurations.Add(new UserMap());
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+
+    public class AppDbContextInitializer : DropCreateDatabaseAlways<AppDbContext>
+    {
+
+    }
+}
---
+++ balta.io/EntityFrameworkGuids/EntityFrameworkGuids/Program.cs
@@ -0,0 +1,53 @@
+﻿using EntityFrameworkGuids.DataContext;
+using EntityFrameworkGuids.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFrameworkGuids
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var andrebaltieri = new User
+            {
+                Email = "andrebaltieri@hotmail.com",
+                IsActive = true,
+                LastUpdateDate = DateTime.Now,
+                LastUpdateUser = "andrebaltieri",
+                Password = "andrebaltieri"
+            };
+
+            var batman = new User
+            {
+                Email = "bruce.wayne@hotmail.com",
+                IsActive = true,
+                LastUpdateDate = DateTime.Now,
+                LastUpdateUser = "andrebaltieri",
+                Password = "batman"
+            };
+
+            using (AppDbContext db = new AppDbContext())
+            {
+                db.Users.Add(andrebaltieri);
+                db.Users.Add(batman);
+                db.SaveChanges();
+
+                foreach (User user in db.Users)
+                {
+                    Console.WriteLine(String.Format("{0} - {1}", user.Id, user.Email));
+                }
+
+                Console.WriteLine(
+                    String.Format("{0} - {1}",
+                    db.Users.Find(batman.Id).Id,
+                    db.Users.Find(batman.Id).Email));
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
